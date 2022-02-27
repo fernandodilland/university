1af04470c36dffd7255ef2f0331ac989d63447f1
Fernando Dilland Mireles Cisneros <fernandodilland@gmail.com>
2022-02-26 23:17:05 -0600

    Finalización

--- Visualización de datos y desarrollo de escritorio/Evidencia 1/Evidencia1-EquipoDinamita.py
+++ Visualización de datos y desarrollo de escritorio/Evidencia 1/Evidencia1-EquipoDinamita.py
@@ -1,72 +1,105 @@
 # Importación de módulos
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd 
+import pandas as pd
 
 # Construcción de diccionario con datos de imagen proporcionada
-"""
 dict_datos = {
     "Años": [2015, 2016, 2017, 2018, 2019], # Coordenada "X" (Horizontal)
     "Matrículas": [181033,190169,197381,202039,206640] # Coordenada "Y" (Vertical)
 }
 
 # Generación de gráfica de Barra
-
 grafica = plt.bar(dict_datos["Años"],dict_datos["Matrículas"])
 plt.ylim(170000, None) # Se define inicio de coordenada "Y" (Vertical)
-plt.title('Matrículas')
-plt.xlabel('Años')
-plt.ylabel('Cantidad')
-#plt.show()
+plt.title('Matrículas') # Título de la gráfica
+plt.xlabel('Años') # Título coordenada "X" (Horizontal)
+plt.ylabel('Cantidad') # Título coordenada "Y" (Vertical)
+plt.show() # Mostrar gráfica
 
 # Generación de gráfica de Línea
-
 grafica = plt.plot(dict_datos["Años"],dict_datos["Matrículas"])
 plt.ylim(170000, None) # Se define inicio de coordenada "Y" (Vertical)
-plt.title('Matrículas')
-plt.xlabel('Años')
-plt.ylabel('Cantidad')
-#plt.show()  # display
+plt.title('Matrículas') # Título de la gráfica
+plt.xlabel('Años') # Título coordenada "X" (Horizontal)
+plt.ylabel('Cantidad') # Título coordenada "Y" (Vertical)
+plt.show() # Mostrar gráfica
 
 # Generación de gráfica de Pie
-
-grafica = plt.pie(dict_datos["Matrículas"],
-                explode=[0, 0, 0, 0, 0.2],
-                labels = dict_datos["Años"],
+grafica = plt.pie(dict_datos["Matrículas"], # Datos a graficar
+                explode=[0, 0, 0, 0, 0.2], # Separar cierta sección
+                labels = dict_datos["Años"], # Títulos en cada sección
                 autopct='%.1f%%')
-plt.title('Matrículas')
-#plt.show()
-"""
+plt.title('Matrículas')  # Título de la gráfica
+plt.show() # Mostrar gráfica
+
+
 # Gráfica de disperción con base a filtro
 # Buscando la variación de la cuenta que pagan las mujeres, en la cena de los sábados, comparar cuanto pagan las que fuman y las que no fuman
-df2 = pd.read_csv("tips.csv")
-df2.head()
-plt.scatter(df2['total_bill'][df2.sex == 'Female'][df2.day == 'Sat'][df2.time == 'Dinner'][df2.smoker == 'No'],
-            df2['tip'][df2.sex == 'Female'][df2.day == 'Sat'][df2.time == 'Dinner'][df2.smoker == 'No'],
+datos = pd.read_csv("tips.csv") # Lectura del archivo .csv
+datos.head()
+plt.scatter(datos['total_bill'][datos.sex == 'Female'][datos.day == 'Sat'][datos.time == 'Dinner'][datos.smoker == 'No'],
+            datos['tip'][datos.sex == 'Female'][datos.day == 'Sat'][datos.time == 'Dinner'][datos.smoker == 'No'],
             color='blue',
             label='No fumadoras')
-plt.scatter(df2['total_bill'][df2.sex == 'Female'][df2.day == 'Sat'][df2.time == 'Dinner'][df2.smoker == 'Yes'],
-            df2['size'][df2.sex == 'Female'][df2.day == 'Sat'][df2.time == 'Dinner'][df2.smoker == 'Yes'],
+plt.scatter(datos['total_bill'][datos.sex == 'Female'][datos.day == 'Sat'][datos.time == 'Dinner'][datos.smoker == 'Yes'],
+            datos['size'][datos.sex == 'Female'][datos.day == 'Sat'][datos.time == 'Dinner'][datos.smoker == 'Yes'],
             color='red',
             label='Fumadoras')
-plt.xlabel('Factura total')
-plt.ylabel('Propina')
+plt.xlabel('Factura total') # Título coordenada "X" (Horizontal)
+plt.ylabel('Propina') # Título coordenada "Y" (Vertical)
 plt.legend()
-plt.show()
+plt.show() # Mostrar gráfica
 
 # Gráfica de disperción con base a filtro
 # Comprobar es cual es la variación de la cuenta que pagan los hombres, en la comida de los jueves, comparar cuanto pagan los que fuman y los que no fuman.
-df2 = pd.read_csv("tips.csv")
-df2.head()
-plt.scatter(df2['total_bill'][df2.sex == 'Male'][df2.day == 'Thur'][df2.time == 'Lunch'][df2.smoker == 'No'],
-            df2['tip'][df2.sex == 'Male'][df2.day == 'Thur'][df2.time == 'Lunch'][df2.smoker == 'No'],
+datos = pd.read_csv("tips.csv") # Lectura del archivo .csv
+datos.head()
+plt.scatter(datos['total_bill'][datos.sex == 'Male'][datos.day == 'Thur'][datos.time == 'Lunch'][datos.smoker == 'No'],
+            datos['tip'][datos.sex == 'Male'][datos.day == 'Thur'][datos.time == 'Lunch'][datos.smoker == 'No'],
             color='blue',
             label='No fumadores')
-plt.scatter(df2['total_bill'][df2.sex == 'Male'][df2.day == 'Thur'][df2.time == 'Lunch'][df2.smoker == 'Yes'],
-            df2['size'][df2.sex == 'Male'][df2.day == 'Thur'][df2.time == 'Lunch'][df2.smoker == 'Yes'],
+plt.scatter(datos['total_bill'][datos.sex == 'Male'][datos.day == 'Thur'][datos.time == 'Lunch'][datos.smoker == 'Yes'],
+            datos['size'][datos.sex == 'Male'][datos.day == 'Thur'][datos.time == 'Lunch'][datos.smoker == 'Yes'],
             color='red',
             label='Fumadores')
-plt.xlabel('Factura total')
-plt.ylabel('Propina')
-plt.legend()
-plt.show()
+plt.xlabel('Factura total') # Título coordenada "X" (Horizontal)
+plt.ylabel('Propina') # Título coordenada "Y" (Vertical)
+plt.legend() # Mostrar recuadro con significado de puntos
+plt.show() # Mostrar gráfica
+
+# Gráfica (subplots) con gráficas anteriores
+datos = pd.read_csv("tips.csv") # Lectura del archivo .csv
+datos.head()
+plt.subplot(1, 2, 1)
+plt.scatter(datos['total_bill'][datos.sex == 'Female'][datos.day == 'Sat'][datos.time == 'Dinner'][datos.smoker == 'No'],
+            datos['tip'][datos.sex == 'Female'][datos.day == 'Sat'][datos.time == 'Dinner'][datos.smoker == 'No'],
+            color='blue',
+            label='No fumadoras')
+plt.scatter(datos['total_bill'][datos.sex == 'Female'][datos.day == 'Sat'][datos.time == 'Dinner'][datos.smoker == 'Yes'],
+            datos['size'][datos.sex == 'Female'][datos.day == 'Sat'][datos.time == 'Dinner'][datos.smoker == 'Yes'],
+            color='red',
+            label='Fumadoras')
+print("Cantidad 1:",len(datos['total_bill'][datos.sex == 'Female'][datos.day == 'Sat'][datos.time == 'Dinner'][datos.smoker == 'No'])+len(datos['total_bill'][datos.sex == 'Female'][datos.day == 'Sat'][datos.time == 'Dinner'][datos.smoker == 'Yes']))
+plt.xlabel('Factura total') # Título coordenada "X" (Horizontal)
+plt.ylabel('Propina') # Título coordenada "Y" (Vertical)
+plt.legend() # Mostrar recuadro con significado de puntos
+plt.title("Mujeres")
+
+plt.subplot(1, 2, 2)
+plt.scatter(datos['total_bill'][datos.sex == 'Male'][datos.day == 'Thur'][datos.time == 'Lunch'][datos.smoker == 'No'],
+            datos['tip'][datos.sex == 'Male'][datos.day == 'Thur'][datos.time == 'Lunch'][datos.smoker == 'No'],
+            color='blue',
+            label='No fumadores')
+plt.scatter(datos['total_bill'][datos.sex == 'Male'][datos.day == 'Thur'][datos.time == 'Lunch'][datos.smoker == 'Yes'],
+            datos['size'][datos.sex == 'Male'][datos.day == 'Thur'][datos.time == 'Lunch'][datos.smoker == 'Yes'],
+            color='red',
+            label='Fumadores')
+print("Cantidad 2:",len(datos['total_bill'][datos.sex == 'Male'][datos.day == 'Thur'][datos.time == 'Lunch'][datos.smoker == 'No'])+len(datos['total_bill'][datos.sex == 'Male'][datos.day == 'Thur'][datos.time == 'Lunch'][datos.smoker == 'Yes']))
+plt.xlabel('Factura total') # Título coordenada "X" (Horizontal)
+plt.ylabel('Propina') # Título coordenada "Y" (Vertical)
+plt.legend() # Mostrar recuadro con significado de puntos
+plt.title("Hombres")
+
+plt.suptitle("Subplots")
+plt.show() # Mostrar gráficas en formato Subplots
